67078750e7919515138a1b809b1812fe1daad849
Samyadeep Basu <f2013401@pilani.bits-pilani.ac.in>
2016-12-23 01:30:21 +0530

    Updated test code

--- review_classification.py
+++ review_classification.py
@@ -1,4 +1,4 @@
-####################################### Classification of Reviews using Bag of Words Model ##############################
+######################################################### Classification of Reviews using Bag of Words Model #############################################
 
 
 ### Libraries for operations ###
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 import re
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
 
 
 #Reading into the panda dataframe  - quoting = 3 helps in ignoring the double quotes, header=0 ensures the first row consists of the headings
@@ -66,15 +67,38 @@ for i in xrange(0,number_of_reviews):
 #Before that the reviews have to be converted to numerical / feature format to apply ML algorithms
 
 #Initialization of CountVectorizer for features
-vectorizer = CountVectorizer(analyzer = "word", tokenizer = None, preprocessor = None, stop_words = None, max_features = 5000)
+vectorizer = CountVectorizer(analyzer = "word", tokenizer = None, preprocessor = None, stop_words = None, max_features = 1000)
 
 #Curation of training features
 training_features = vectorizer.fit_transform(cleaned_reviews)
 
 #Conversion into numpy array for easy processing for ML algorithms
-features = np.array(training_features)
-print features
+features = training_features.toarray()
 
+#Initialising the Random Forest Classifier - 100 Trees 
+forest = RandomForestClassifier(n_estimators = 100)
+
+#Providing the model with training data - labelled
+forest = forest.fit(features, train["sentiment"])
+
+#Reading the test data into dataFrame
+test= pd.read_csv("testData.tsv", header=0, delimiter="\t", quoting=3)
+
+n_reviews = len(test["review"])
+
+clean_test_reviews = []
+
+for j in xrange(0,n_reviews):
+	clean_test_reviews.append(conversion_to_words(test['review'][j]))
+
+
+test_data_features = vectorizer.fit_transform(clean_test_reviews)
+
+test_data_features = test_data_features.toarray()
+
+result = forest.predict(test_data_features)
+
+print result
 
 
 
